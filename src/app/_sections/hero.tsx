2d439bea1661123d033f9cc29bd5de6dadbacc9a
MichaelWood <mgwood22@sky.com>
2024-03-22 15:14:49 +0000

    added CV button"

--- src/app/_sections/hero.tsx
+++ src/app/_sections/hero.tsx
@@ -1,32 +1,48 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import NewTabLink from "@/components/newTabLink";
 
 const Hero = () => {
   return (
     <div className="w-full min-h-screen flex flex-col lg:flex-row justify-center items-center gap-10 px-4 lg:px-0">
       {/* Text box */}
       <div className="lg:w-1/2 w-full text-center lg:text-left bg-white shadow-xl rounded-lg p-6 flex justify-center items-center">
-        <span className='font-bold text-center'>
-          <h1 className='text-xl text-honblue px-3'> Hello, I'm</h1> 
-          <h2 className='text-4xl font-extrabold text-fedblue px-3'> Michael Wood</h2>
-          <h3 className='text-xl text-honblue px-3'>I am a Full Stack Developer specializing in Node, Tailwind CSS, TypeScript, Next.js and MongoDB.</h3>
-        </span> 
+        <span className="font-bold text-center">
+          <h1 className="text-xl text-honblue px-3"> Hello, I'm</h1>
+          <h2 className="text-4xl font-extrabold text-fedblue px-3">
+            {" "}
+            Michael Wood
+          </h2>
+          <h3 className="text-xl text-honblue px-3">
+            I am a Full Stack Developer specializing in Node, Tailwind CSS,
+            TypeScript, Next.js and MongoDB.
+          </h3>
+        </span>
       </div>
-      <a href='https://github.com/Mike-W00d'>
-      <Image src='/github.png'
-        alt='Github logo' 
-        width={40}
-        height={40}
-        /> 
-        </a>
-        <Button className='bg-fedblue hover:bg-honblue'>Download CV</Button>
+      <a href="https://github.com/Mike-W00d">
+        <Image src="/github.png" alt="Github logo" width={40} height={40} />
+      </a>
+      <Button
+        className="bg-fedblue hover:bg-honblue"
+        onClick={() => window.open("/CV.pdf", "_blank", "noopener noreferrer")}
+      >
+        Download CV
+      </Button>
       {/* Image */}
       <div className="lg:w-1/2 w-full textå-center relative flex justify-center items-center">
-        <Image src="/hero.png" alt="Hero" width={450} height={550} className="rounded-full" /> 
+        <Image
+          src="/hero.png"
+          alt="Hero"
+          width={450}
+          height={550}
+          className="rounded-full"
+        />
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
---
+++ src/components/newTabLink.tsx
@@ -0,0 +1,16 @@
+import React, { FC, ReactNode } from 'react';
+
+interface NewTabLinkProps {
+  href: string;
+  children: ReactNode;
+}
+
+const NewTabLink: FC<NewTabLinkProps> = ({ href, children }) => {
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  );
+};
+
+export default NewTabLink;
