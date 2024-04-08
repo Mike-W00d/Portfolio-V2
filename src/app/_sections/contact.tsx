2bb57380e0402b4b915a115f29c8abeb77a686b6
MichaelWood <mgwood22@sky.com>
2024-04-08 11:35:52 +0100

    contact added

--- src/app/_sections/contact.tsx
+++ src/app/_sections/contact.tsx
@@ -1,8 +1,11 @@
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div className="flex px-10 justify-center mt-10">
+      <div className="bg-white shadow-xl rounded-full p-8 w-full max-w-4xl">
+      <h1 className="text-xl font-bold text-fedblue text-center mb-6">Get in Touch</h1>
+      <span> Email - mgwood22@sky.com </span>
     </div>
+  </div>
   )
 }
 
