ac8e36eb169d461d335293cd6963fc958f18665c
MichaelWood <mgwood22@sky.com>
2024-06-17 11:59:18 +0100

    Gallery Images fetch and display

--- src/app/(Pages)/gallery/page.tsx
+++ src/app/(Pages)/gallery/page.tsx
@@ -1,31 +1,33 @@
 import React from 'react'
-import { CldImage } from 'next-cloudinary'
-import cloudinary from '@/cloudinary/config'
-
-import { useState, useEffect } from 'react'
+import cloudinary from 'cloudinary'
+import { CloudinaryImage } from '@/components/cloudinaryIMG'
 
 
-export default function Gallery () {
-  // const [resources, setResources] = useState(null)
+type SearchResults = {
+  public_id: string;
+  secure_url: string;
+}
+
+export default async function Gallery () {
+  const results = await cloudinary.v2.search
+  .expression('mgmwood.com')
+  .sort_by('public_id','desc')
+  .max_results(30)
+  .execute() as {resources: SearchResults[]}
 
   return (
-      // <ul className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-      //   {resources.map((image: CloudinaryImage) => {
-      //     return (
-      //       <li key={image.public_id}>
-      //         <CldImage
-      //           src={image.secure_url}
-      //           width='300'
-      //           height='300'
-      //           crop='fill'
-      //           alt='Gallery Image'
-      //         />
-      //       </li>
-      //     )
-      //   })
-      //   }
-      // </ul>
-      <div>
-        Page Coming Soon
+    <section>
+      <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        {results.resources.map((result) => (
+          <CloudinaryImage 
+          key={result.public_id}
+          src={result.secure_url}
+          alt= "An Image of something"
+          width= "300"
+          height= "300"
+          />
+        ))}
       </div>
-  )}
+    </section>
+  )
+}
