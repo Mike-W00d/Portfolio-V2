7b505cb397e4f4ecc3ed2b7c32dff79e20e672d5
MichaelWood <mgwood22@sky.com>
2024-09-17 17:04:50 +0100

    bug fixes for deployment

--- src/app/(Pages)/gallery/page.tsx
+++ src/app/(Pages)/gallery/page.tsx
@@ -2,7 +2,36 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import {
+  RenderImageContext,
+  RenderImageProps,
+  RowsPhotoAlbum,
+} from "react-photo-album";
+import "react-photo-album/rows.css";
 
+function renderNextImage(
+  { alt = "", title, sizes }: RenderImageProps,
+  { photo, width, height }: RenderImageContext,
+) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        position: "relative",
+        aspectRatio: `${width} / ${height}`,
+      }}
+    >
+      <Image
+        fill
+        src={photo}
+        alt={alt}
+        title={title}
+        sizes={sizes}
+        placeholder={"blurDataURL" in photo ? "blur" : undefined}
+      />
+    </div>
+  );
+}
 
 export default function Page() {
   const fetchPhotos = async () => {
