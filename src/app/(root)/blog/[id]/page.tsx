aac3a77a8f11bad19359336c6a288d6864087802
MichaelWood <mgwood22@sky.com>
2025-02-17 15:11:54 +0000

    post by id page works correctly

--- src/app/(root)/blog/[id]/page.tsx
+++ src/app/(root)/blog/[id]/page.tsx
@@ -1,6 +1,10 @@
 // import { Metadata } from "next";
 // import { notFound } from "next/navigation";
 
+import { title } from "process";
+
+import Image from "next/image";
+
 // import Container from "@/components/blog-components/container";
 // import { getAllPosts, getPostBySlug } from "@/lib/api";
 // import { CMS_NAME } from "@/lib/constants";
@@ -72,5 +76,28 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  return <div>My Post: {id}</div>;
+  const fetchPost = async (id: string) => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/posts/${id}`,
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch post");
+    }
+    return response.json();
+  };
+
+  const data = await fetchPost(id);
+
+  const { title, content, coverImage, excerpt, date, author } = data.data;
+
+  return (
+    <main>
+      <h1> {title} </h1>
+      <Image src={coverImage} alt={title} width={200} height={200} />
+      <p> {excerpt} </p>
+      <p> {date} </p>
+      <p> {author} </p>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </main>
+  );
 }
