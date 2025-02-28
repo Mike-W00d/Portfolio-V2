b87bd8313210580c5bb0e80bfe691a7a1380d6fd
MichaelWood <mgwood22@sky.com>
2025-02-28 16:34:37 +0000

    date formats correctly, hero post comes from database rather than mock

--- src/components/heroPost.tsx
+++ src/components/heroPost.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import moment from "moment";
 import Image from "next/image";
-import { Preview } from "./editor/Preview";
 import Link from "next/link";
+import React from "react";
+
 import Avatar from "./blog-components/avatar";
+import { Preview } from "./editor/Preview";
 
 interface PostContainerProps {
   id: string;
@@ -13,33 +15,52 @@ interface PostContainerProps {
   content: string;
 }
 
-const HeroPost = ({ id, title, coverImage, content }: PostContainerProps) => {
+const HeroPost = ({
+  id,
+  title,
+  coverImage,
+  content,
+  date,
+}: PostContainerProps) => {
+  function formatDateString(dateString: string) {
+    return moment(dateString).format("MMMM Do YYYY");
+  }
+  const formattedDate = formatDateString(date);
   return (
-    <div className="flex flex-col w-full max-h-[386px]">
-      <div className="flex space-x-2 justify-between">
-        <div className="w-1/2">
+    <div className="flex max-h-[386px] w-full flex-col">
+      <div className="flex justify-between space-x-2">
+        <div className="relative w-1/2">
           <Link className="w-full cursor-pointer" href={`/blog/${id}`}>
-            <Image src={coverImage} alt={title} width={600} height={900} />
+            <Image
+              src={coverImage}
+              alt={title}
+              fill
+              sizes="(max-width: 556px) 100vw"
+            />
           </Link>
         </div>
-        <div className="text-right w-1/2 max-h-[386px]">
-          <h1 className="text-3xl text-fedblue font-bold hover:underline cursor-pointer mb-3">
+        <div className="max-h-[386px] w-1/2 text-right">
+          <h1 className="mb-3 cursor-pointer text-3xl font-bold text-fedblue hover:underline">
             {title}
           </h1>
-          <h2 className="text-honblue font-bold"> </h2>
+          <h2 className="font-bold text-honblue"> </h2>
           <div className="h-[250px] overflow-hidden">
             <Preview content={content} />
           </div>
-          <div className="flex justify-between">
-            <Avatar name="John Doe" picture="/HERO2.png" />
-            <Link href={`/blog/${id}`}>
-              <h1 className="text-2xl text-fedblue cursor-pointer hover:underline">
-                {" "}
-                Read the full story{" "}
-              </h1>
-            </Link>
+          <div className="flex items-center justify-between object-bottom">
+            <Avatar name="Michael Wood" picture="/HERO2.png" />
+            <div className="flex flex-col items-end">
+              <Link href={`/blog/${id}`}>
+                <h1 className="cursor-pointer text-2xl text-fedblue hover:underline">
+                  {" "}
+                  Read the full story{" "}
+                </h1>
+              </Link>
+              <p className="text-sm text-honblue"> {formattedDate} </p>
+            </div>
           </div>
         </div>
+        å
       </div>
     </div>
   );
--- src/components/postContainer.tsx
+++ src/components/postContainer.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import Image from "next/image";
+"use client";
+
+import moment from "moment";
 import Link from "next/link";
+import { CldImage } from "next-cloudinary";
+import React from "react";
 
 interface PostContainerProps {
   key?: string;
@@ -18,21 +21,26 @@ const PostContainer = ({
   excerpt,
   date,
 }: PostContainerProps) => {
+  function formatDateString(dateString: string) {
+    return moment(dateString).format("MMMM Do YYYY");
+  }
+  const formattedDate = formatDateString(date);
   return (
-    <div className="w-9/10 bg-white shadow-xl rounded-md flex flex-col items-center justify-center mx-4 my-8">
+    <div className="mx-4 my-8 flex flex-col items-center justify-center rounded-md bg-white shadow-xl">
       <Link href={`/blog/${id}`}>
-        <div className="w-full max-h-[400px] relative flex justify-center">
-          <Image
+        <div className="relative flex max-h-[400px] min-h-[200px]  w-full justify-center px-4">
+          <CldImage
             src={coverImage}
             alt={title}
-            width={500}
-            height={500}
-            objectFit="contain"
+            height={600}
+            width={800}
+            sizes="100vw"
           />
         </div>
-        <div className="p-4 flex flex-col items-center text-center">
-          <h1 className="text-fedblue text-3xl font-bold"> {title} </h1>
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-3xl font-bold text-fedblue"> {title} </h1>
           <p className="text-lg text-honblue"> {excerpt} </p>
+          <p className="text-lg text-honblue">{formattedDate}</p>
         </div>
       </Link>
     </div>
