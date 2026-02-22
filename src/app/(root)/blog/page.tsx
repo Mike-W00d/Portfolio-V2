2a37fc9a9b103424a1eb8bd9a7bacad4fed1e1a9
MichaelWood <mgwood22@sky.com>
2026-02-22 16:26:16 +0000

    fixing conflicting dependancies

--- src/app/(root)/blog/page.tsx
+++ src/app/(root)/blog/page.tsx
@@ -1,9 +1,36 @@
 import { Metadata } from "next";
 
+import Container from "@/components/blog-components/container";
+import { HeroPost } from "@/components/blog-components/hero-post";
+import { Intro } from "@/components/blog-components/intro";
+import { MoreStories } from "@/components/blog-components/more-stories";
+import { getAllPosts } from "@/lib/api";
+
 export const metadata: Metadata = {
   title: "Blog",
 };
 
-export default function Page() {
-  return <div>Blog Coming Soon</div>;
+export default function Index() {
+  const allPosts = getAllPosts();
+
+  const heroPost = allPosts[0];
+
+  const morePosts = allPosts.slice(1);
+
+  return (
+    <main>
+      <Container>
+        <Intro />
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
+    </main>
+  );
 }
